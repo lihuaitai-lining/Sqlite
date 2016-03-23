34f5752508e32c1eab501070030d9f73e566b90d
lihuaitai <lihuaitai@lihuaitaideMac-mini.local>
2016-03-23 09:27:30 +0800

    Initial Commit

---
+++ Sqlit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sqlit
+//
+//  Created by 李怀泰 on 16/3/23.
+//  Copyright © 2016年 李怀泰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sqlit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sqlit
+//
+//  Created by 李怀泰 on 16/3/23.
+//  Copyright © 2016年 李怀泰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
